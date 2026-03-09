896dfb81604d6e36f6621f96115d8e6124ec7d70
khsydney-splunk <hyungk@splunk.com>
2026-03-09 12:13:03 +0800

    Replace sensitive Splunk token and app name with example
    Updated Splunk configuration with placeholder values for TOKEN and APP_NAME.

--- instrumentation/android-astronomy-shop/app/src/main/java/com/astronomyshop/app/utils/SplunkConfiguration.kt
+++ instrumentation/android-astronomy-shop/app/src/main/java/com/astronomyshop/app/utils/SplunkConfiguration.kt
@@ -4,11 +4,11 @@ object SplunkConfiguration {
 
     // Replace with your Splunk realm and RUM access token
     const val REALM = "us1"
-    const val TOKEN = "EX2GzzP81oW5y9rhcO0Oyg"
+    const val TOKEN = "your_RUM_token"
 
     const val ENVIRONMENT = "test"
     const val MOBILE_APP_VERSION = "1.0.0"
-    const val APP_NAME = "Nick-Astronomy-splunk"
+    const val APP_NAME = "Application Name"
 
     val ENDPOINT_URL = "https://rum-ingest.$REALM.signalfx.com/v1/rum"
 
